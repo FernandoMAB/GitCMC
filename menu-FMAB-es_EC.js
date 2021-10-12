138acd8140633ab201935e2d1bbcbb159dbbe86d
FernandoMAB <62525270+FernandoMAB@users.noreply.github.com>
2021-10-12 16:46:47 -0500

    Menus

---
+++ menu-FMAB-es_EC.js
@@ -0,0 +1,21 @@
+{
+    "COMMONS": {
+        "MENU": {
+			"MNU_FMAB_BVDA":"Bienvenida Fernando Mejia",
+			"MNU_SMCMC":"Formulario Semillero",
+            "MNU_FMAB_RPS":"Formulario de Repaso",
+            "MNU_FMAB_EJER":"Formulario Ejercicio",
+            "MNU_FMAB_EJER1":"Calificaciones Fernando Mejia",
+            "MNU_FMAB_EJER2":"Calculadora Fernando Mejia",
+            "MNU_FMAB_EVA2":"Evaluacion2 Fernando Mejia",
+            "MNU_FMAB_BLI":"Formulario BLI",
+            "MNU_FMAB_TNDA":"Tienda FMAB BLI",
+            "MNU_FMAB_TNDR":"Recuperar Tiendas BLI",
+            "MNU_FMAB":"Evaluacion FMAB",
+            "MNU_FMAB_EVI":"Ingresar Producto",
+            "MNU_FMAB_EVR":"Productos",
+            "MNU_FMAB_EV2":"Recuperar Clientes"
+            
+        }
+    }
+}
